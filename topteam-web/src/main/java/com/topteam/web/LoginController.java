03f07bca3abc4ddcf9c2ff58cf38660821802593
cww0123 <cww93@sina.com>
2016-10-19 00:08:08 +0800

    新建Login

--- topteam-web/src/main/java/com/topteam/web/LoginController.java
+++ topteam-web/src/main/java/com/topteam/web/LoginController.java
@@ -1,7 +1,14 @@
 package com.topteam.web;
 
+import com.topteam.common.AjaxResult;
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by chengwenwen on 2016/10/18.
  */
+@Controller
 public class LoginController {
+
+    public void test(){
+    }
 }
