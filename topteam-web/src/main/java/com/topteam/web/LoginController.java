9cdfceec592d9ad30a75502c7c71dc3c2dd9f17b
cww0123 <cww93@sina.com>
2016-10-27 23:50:06 +0800

    新建登陆自定义注解

--- topteam-web/src/main/java/com/topteam/web/LoginController.java
+++ topteam-web/src/main/java/com/topteam/web/LoginController.java
@@ -1,14 +1,27 @@
 package com.topteam.web;
 
-import com.topteam.common.AjaxResult;
+import com.topteam.annotation.LoginCheck;
+import com.topteam.commons.AjaxResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by chengwenwen on 2016/10/18.
  */
 @Controller
+@RequestMapping(value = "/login")
 public class LoginController {
 
-    public void test(){
+    @LoginCheck
+    @ResponseBody
+    @RequestMapping(value = "/login.json")
+    public AjaxResult test(){
+        Map map = new HashMap();
+        map.put("ok","test");
+        return AjaxResult.successResult(map);
     }
 }
